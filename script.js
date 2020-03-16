c9b39b2663e50f92356965e20ad7181a4b931c68
prabhatkumar9 <61245217+prabhatkumar9@users.noreply.github.com>
2020-03-16 13:30:08 +0530

    FSJ0320A003

--- script.js
+++ script.js
@@ -22,3 +22,37 @@
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+
+function checkUserName() {
+    var paswrd = document.getElementById('passwordtext').value;
+    var name = document.getElementById('usertext').value;
+    var email = document.getElementById('emailtext').value;
+    var usrnameList = /^[a-zA-Z0-9]+$/;
+    
+    if (!usrnameList.test(name))
+     {
+        alert("Please enter username");
+        return false;
+    }
+    
+    if(name.length < 8 || name.length > 15) {
+        alert("Username shoud be between 8 to 15 characters")
+    } 
+    
+    var emailList = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailList.test(email))
+     {
+        alert("Oops, this doesn't looks like a email.");
+        return false;
+    }
+    var pwrdList = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    if (!pwrdList.test(paswrd))
+     {
+        alert("Please enter your password");
+        return false;
+    }
+    alert("Registration successfull, Now you can proceed to login!");
+
+    return true;    
+}
+
